6eab6f0de70d89d7d7c92f1d7a22c4e118eb7484
PotcFdk <PotcFdk@gmx.net>
2015-07-05 23:20:45 +0200

    Add the userscript.

---
+++ SteamLinkFilterBypass.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @author      PotcFdk
+// @name        Steam Link-Filter Bypass
+// @namespace   https://github.com/PotcFdk/SteamLinkFilterBypass
+// @description Skips the Steam LinkFilter.
+// @include     https://steamcommunity.com/linkfilter/*
+// @include     http://steamcommunity.com/linkfilter/*
+// @version     0.0.1
+// @grant       none
+// @downloadURL https://raw.githubusercontent.com/PotcFdk/SteamLinkFilterBypass/master/SteamLinkFilterBypass.user.js
+// @updateURL   https://raw.githubusercontent.com/PotcFdk/SteamLinkFilterBypass/master/SteamLinkFilterBypass.meta.js
+// ==/UserScript==
+
+/*
+	Steam LinkFilter Bypass - Copyright (c) PotcFdk, 2015
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+location.search.substr (1).split ('&').forEach (function (param) {
+	if (param.split ('=')[0] == "url") {
+		location.href = param.split ('=')[1];
+	}
+});
